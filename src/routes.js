21d4d30463988182b4c430107b1a3bc26eb673fe
Mateus Mangueira <mateus.mangueira@ccc.ufcg.edu.br>
2019-10-16 20:42:39 -0300

    Configurando a migration e criando model do User

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,14 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello World' }));
-
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Mateus Mangueira',
+    email: 'mateus.mangueira14@gmail.com',
+    password_hash: '123321',
+  });
+  return res.json(user);
+});
 export default routes;
