a97d7dd985ac783b52afe1afddfa330700d9ffb4
Mateus Mangueira <mateus.mangueira@ccc.ufcg.edu.br>
2019-10-17 16:06:02 -0300

    Validacao quase funcionando, bug em update (email nao consegue ser pego)

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -1,17 +1,22 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
-  async index(req, res) {
-    // Completar a logica de retornar todos os usuarios
-    return res.json();
-  }
-
-  async show(req, res) {
-    // Completar logica de retornar apenas um unico usuario pelo ID
-    return res.json();
-  }
-
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(6),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation Failed' });
+    }
+
     const userExists = await User.findOne({ where: { email: req.body.email } });
 
     if (userExists) {
@@ -28,12 +33,41 @@ class UserController {
   }
 
   async update(req, res) {
-    return res.json({ ok: true });
-  }
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
 
-  async delete(req, res) {
-    // Completar a logica de deletar um usuario pelo ID
-    return res.json();
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed' });
+    }
+
+    const { email, oldPassword } = req.body;
+    const user = await User.findByPk(req.userId);
+    if (user.email !== email) {
+      const userExists = await User.findOne({
+        where: { email },
+      });
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists.' });
+      }
+    }
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match.' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({ id, name, email, provider });
   }
 }
 
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -6,9 +6,10 @@ import authMiddleware from './app/middlewares/auth';
 const routes = new Router();
 
 routes.post('/users', UserController.store);
-routes.get('/users', UserController.show);
-routes.put('/users', authMiddleware, UserController.update);
-
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 export default routes;
