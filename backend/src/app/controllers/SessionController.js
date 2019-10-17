3dd65347afce822338a019f3563beeefb21e0eab
Mateus Mangueira <mateus.mangueira@ccc.ufcg.edu.br>
2019-10-17 12:17:31 -0300

    Criando autenticacao com JWT e SessionController

---
+++ backend/src/app/controllers/SessionController.js
@@ -0,0 +1,33 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/User';
+
+class SessionController {
+  async store(req, res) {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(401).json({ error: 'User not found' });
+    }
+
+    if (!(await User.checkPassword(password))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name } = user;
+
+    return res.json({
+      user: {
+        id,
+        name,
+        email,
+      },
+      token: jwt.sign({ id }, 'a2ca9c9dd6cad81544592993224d4257', {
+        expiresIn: '7d',
+      }),
+    });
+  }
+}
+
+export default new SessionController();
--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -2,12 +2,12 @@ import User from '../models/User';
 
 class UserController {
   async index(req, res) {
-   //Completar a logica de retornar todos os usuarios
+    // Completar a logica de retornar todos os usuarios
     return res.json();
   }
 
   async show(req, res) {
-    //Completar logica de retornar apenas um unico usuario pelo ID
+    // Completar logica de retornar apenas um unico usuario pelo ID
     return res.json();
   }
 
@@ -18,22 +18,22 @@ class UserController {
       return res.status(400).json({ error: 'User already exists.' });
     }
 
-    const {id, name, email, provider } = await User.create(req.body);
+    const { id, name, email, provider } = await User.create(req.body);
     return res.json({
       id,
       name,
       email,
-      provider
+      provider,
     });
   }
 
   async update(req, res) {
-    //Completar a logica de atualizar um usuario pelo ID
+    // Completar a logica de atualizar um usuario pelo ID
     return res.json();
   }
 
   async delete(req, res) {
-    //Completar a logica de deletar um usuario pelo ID
+    // Completar a logica de deletar um usuario pelo ID
     return res.json();
   }
 }
