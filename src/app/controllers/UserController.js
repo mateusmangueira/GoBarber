573de6771a1a287bcd6db8a556107d2249506dee
Mateus Mangueira <mateus.mangueira@ccc.ufcg.edu.br>
2019-10-16 21:08:59 -0300

    Criando o UserController e iniciando o CRUD para user

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,41 @@
+import User from '../models/User';
+
+class UserController {
+  async index(req, res) {
+   //Completar a logica de retornar todos os usuarios
+    return res.json();
+  }
+
+  async show(req, res) {
+    //Completar logica de retornar apenas um unico usuario pelo ID
+    return res.json();
+  }
+
+  async store(req, res) {
+    const userExists = await User.findOne({ where: { email: req.body.email } });
+
+    if (userExists) {
+      return res.status(400).json({ error: 'User already exists.' });
+    }
+
+    const {id, name, email, provider } = await User.create(req.body);
+    return res.json({
+      id,
+      name,
+      email,
+      provider
+    });
+  }
+
+  async update(req, res) {
+    //Completar a logica de atualizar um usuario pelo ID
+    return res.json();
+  }
+
+  async delete(req, res) {
+    //Completar a logica de deletar um usuario pelo ID
+    return res.json();
+  }
+}
+
+export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Mateus Mangueira',
-    email: 'mateus.mangueira14@gmail.com',
-    password_hash: '123321',
-  });
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
+routes.get('/users', UserController.show);
+
 export default routes;
