d2f2f9820efc7b702e46b651ee75f164de59b939
Mateus Mangueira <mateus.mangueira@ccc.ufcg.edu.br>
2019-10-19 14:14:49 -0300

    Listagem dos agendamentos de um usuario pronta

--- backend/src/app/controllers/AppointmentController.js
+++ backend/src/app/controllers/AppointmentController.js
@@ -1,8 +1,30 @@
 import Appointment from '../models/Appointment';
 import { startOfHour, parseISO, isBefore } from 'date-fns';
 import User from '../models/User';
+import File from '../models/File';
 import * as Yup from 'yup';
+
 class AppointmentController {
+
+  async index(req, res) {
+    const appointments = await Appointment.findAll({
+      where: { user_id: req.userId, canceled_at: null },
+      order: ['date'],
+      attributes: ['id','date'],
+      include:[{
+        model: User,
+        as: 'provider',
+        attributes: ['id', 'name'],
+        include: [{
+          model: File, 
+          as: 'avatar',
+          attributes: ['id','path','url']
+        }]
+      }]
+    });
+
+    return res.json(appointments);
+  }
 
   async store(req, res) {
     const schema = Yup.object().shape({
