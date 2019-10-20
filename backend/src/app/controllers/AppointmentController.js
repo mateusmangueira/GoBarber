42843532ee41e54f92f58cbaa6bb34eadfea3b3c
Mateus Mangueira <mateus.mangueira@ccc.ufcg.edu.br>
2019-10-20 20:35:03 -0300

    Envio de email de cancelamento concluido, bug no delete de agendamento ainda continua

--- backend/src/app/controllers/AppointmentController.js
+++ backend/src/app/controllers/AppointmentController.js
@@ -6,6 +6,8 @@ import File from '../models/File';
 import Notification from '../schemas/Notification';
 import * as Yup from 'yup';
 
+import Mail from '../../lib/Mail';
+
 class AppointmentController {
 
   async index(req, res) {
@@ -106,7 +108,15 @@ class AppointmentController {
   };
 
   async delete(req, res) {
-    const appointment = await Appointment.findByPk(req.params.id);
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email'],
+        }
+      ]
+    });
 
     if (appointment.user_id !== req.userId) {
       return res.status(401).json({
@@ -124,6 +134,12 @@ class AppointmentController {
       appointment.canceled_at = new Date();
 
       await appointment.save();
+
+      await Mail.sendMail({
+        to: `${appointment.provider.name} <${appointment.provider.email}>`,
+        subject: 'Agendamento cancelado',
+        text: 'Voce tem um novo cancelamento',
+      })
 
       return res.json(appointment);
     }
---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  secure: false,
+  auth: {
+    user: "e3dd98fb1024ed",
+    pass: "e691140ffefa9f"
+  },
+  default: {
+    from: 'GoBarber Crew <noreply@gobarber.com'
+  },
+}
