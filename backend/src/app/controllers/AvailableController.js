27cc75d6918819ea680772a962992641a54b387b
Mateus Mangueira <mateus.mangueira@ccc.ufcg.edu.br>
2019-10-21 21:36:27 -0300

    Listando horarios disponiveis finalizando, contem 1 bug

--- backend/src/app/controllers/AvailableController.js
+++ backend/src/app/controllers/AvailableController.js
@@ -1,8 +1,62 @@
+import { startOfDay, endOfDay, setHours, setMinutes, setSeconds, format, isAfter } from 'date-fns';
+import { Op } from 'sequelize';
+import Appointment from '../models/Appointment';
+
 class AvailableController {
   async index(req, res) {
-    return res.json({
-      ok: true
+    const { date } = req.query;
+
+    if (!date) {
+      return res.status(400).json({ error: 'Invalid date' });
+    }
+
+    const searchDate = Number(date);
+
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.params.providerId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(searchDate), endOfDay(searchDate)],
+        },
+      },
     });
+
+    //Horarios que o prestador pode trabalhar por dia, se quiser trabalhar mais ou menos so modificar aqui
+
+    const schedule = [
+      '08:00',
+      '09:00',
+      '10:00',
+      '11:00',
+      '12:00',
+      '13:00',
+      '14:00',
+      '15:00',
+      '16:00',
+      '17:00',
+      '18:00',
+      '19:00',
+    ];
+
+    //Se quiser mudar o tempo do agendamento, eh so mudar as horas dentro da lista de schedule; //Na nossa aplicacao o tempo de agendamento eh appointment cada 1 hr, por isso o minute eh 0
+    const available = schedule.map(time => {
+      const [hour, minute] = time.split(':');
+      const value = setSeconds(
+        setMinutes(setHours(searchDate, hour), minute),
+        0
+      );
+
+      return {
+        time,
+        value: format(value, "yyyy-MM-dd'T'HH:mm:ssxxx"),
+        available:
+          isAfter(value, new Date()) &&
+          !appointments.find(appointment => format(appointment.date, 'HH:mm') === time),
+      };
+    });
+
+    return res.json(available);
   }
 }
 export default new AvailableController();
