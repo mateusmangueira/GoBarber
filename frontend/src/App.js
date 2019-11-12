c638effc469d59b6bb14368c31f200f06fc2c0ef
mateusmangueira <mateus.mangueira@ccc.ufcg.edu.br>
2019-11-12 14:39:45 -0300

    Reajustando a configuracao do Reactotron

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
 import history from './services/history';
-import Routes from "./routes";
+import Routes from './routes';
+import './config/ReactotronConfig'
 
 function App() {
   return (
