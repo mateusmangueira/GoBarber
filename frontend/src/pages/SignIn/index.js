67ce42a03dc4a766a75c07dafd455825a51a7c79
mateusmangueira <mateus.mangueira@ccc.ufcg.edu.br>
2019-11-13 20:09:43 -0300

    Estilizacao das pages SingIn e SingUp

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { Link } from 'react-router-dom';
+import logo from '~/assets/logo.svg';
 
 export default function SignIn() {
-  return <h1>Hello SingIn</h1>;
+  return (
+    <>
+      <img src={logo} alt="GoBarber Logo" />
+      <form action="">
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha mais segura" />
+        <button type="submit">Entrar</button>
+        <Link to="/register">Criar sua conta</Link>
+      </form>
+    </>
+  );
 }
--- frontend/src/pages/SingUp/index.js
+++ frontend/src/pages/SingUp/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '~/assets/logo.svg';
 
-// import { Container } from './styles';
-
-export default function SingUp() {
-  return <h1>Hello SingUp</h1>;
+export default function SignUp() {
+  return (
+    <>
+      <img src={logo} alt="GoBarber Logo" />
+      <form action="">
+        <input placeholder="Nome completo" />
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha" />
+        <button type="submit">Criar conta gratuitamente</button>
+        <Link to="/">Já possui login?</Link>
+      </form>
+    </>
+  );
 }
