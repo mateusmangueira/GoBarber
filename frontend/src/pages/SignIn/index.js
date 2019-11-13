9e3b4b1537f53562b2dba191387a2ac09ac2b7bd
mateusmangueira <mateus.mangueira@ccc.ufcg.edu.br>
2019-11-13 20:23:02 -0300

    Formulario de criacao e login prontos

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 import logo from '~/assets/logo.svg';
 
 export default function SignIn() {
+  function handleSingIn(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="GoBarber Logo" />
-      <form action="">
-        <input type="email" placeholder="Seu e-mail" />
-        <input type="password" placeholder="Sua senha mais segura" />
+      <Form onSubmit={handleSingIn}>
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha mais segura"
+        />
         <button type="submit">Entrar</button>
         <Link to="/register">Criar sua conta</Link>
-      </form>
+      </Form>
     </>
   );
 }
