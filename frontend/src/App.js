1adf87eba8f4b4b0c0791f852302b8dc6963849f
mateusmangueira <mateus.mangueira@ccc.ufcg.edu.br>
2019-11-13 18:58:40 -0300

    Layouts padroes e estilos globais implementados

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { Router } from 'react-router-dom';
 import history from './services/history';
 import Routes from './routes';
-import './config/ReactotronConfig'
+import './config/ReactotronConfig';
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <Router history={history}>
       <Routes />
+      <GlobalStyle />
     </Router>
-  )
+  );
 }
 export default App;
