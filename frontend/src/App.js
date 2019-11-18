7e520f90080148c4cafa2a538eb299bdfc2d8250
mateusmangueira <mateus.mangueira@ccc.ufcg.edu.br>
2019-11-18 20:05:19 -0300

    Armazenando o profile e persistindo o login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,22 +2,26 @@ import React from 'react';
 
 import './config/ReactotronConfig';
 
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 import history from './services/history';
 import Routes from './routes';
 
-import store from './store';
+import { persistor, store } from './store';
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <Routes />
-        <GlobalStyle />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+          <GlobalStyle />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
+
 export default App;
