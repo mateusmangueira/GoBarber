d5dc0c53aae0ce954e700cba12bdbefba3b6a96f
mateusmangueira <mateus.mangueira@ccc.ufcg.edu.br>
2019-11-21 13:59:28 -0300

    Pagina de login pronta e iniciando criar conta

--- mobile/src/pages/SignIn/index.js
+++ mobile/src/pages/SignIn/index.js
@@ -1,18 +1,50 @@
 import React from 'react';
-import { Text } from 'react-native';
 
+import { Image } from 'react-native';
+
+import logo from '~/assets/logo.png';
 import Background from '~/components/Background';
-import Input from '~/components/Input';
-import Button from '~/components/Button';
 
-// import { Container } from './styles';
+import { Container, Form, FormInput, SubmitButton, SignLink, SignLinkText } from './styles';
 
 export default function SignIn() {
+
+  async function handleSingUp() {
+
+  }
+
+  async function handleSubmit() {
+
+  }
+
   return (
     <Background>
-      <Text>Hello SignIn</Text>
-      <Input style={{ marginTop: 30 }} icon="call" placeholder="Digite seu nome" />
-      <Button>Aperte Aqui Cuzao</Button>
+      <Container>
+        <Image source={logo} />
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+          />
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha secreta"
+          />
+
+          <SubmitButton onPress={() => { handleSubmit }}>
+            Entrar
+          </SubmitButton>
+        </Form>
+        <SignLink onPress={() => { handleSingUp }}>
+          <SignLinkText>
+            Criar sua conta gratuita
+          </SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 }
--- mobile/src/pages/signUp/index.js
+++ mobile/src/pages/signUp/index.js
@@ -1,14 +1,50 @@
 import React from 'react';
-import { View, Text } from 'react-native';
 
+import { Image } from 'react-native';
+
+import logo from '~/assets/logo.png';
 import Background from '~/components/Background';
 
-// import { Container } from './styles';
+import { Container, Form, FormInput, SubmitButton, SignLink, SignLinkText } from './styles';
 
-export default function SignUp() {
+export default function SignIn() {
   return (
     <Background>
-      <Text>Hello SignUp</Text>
+      <Container>
+        <Image source={logo} />
+        <Form>
+        <FormInput
+            icon="name-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+          />
+
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha segura"
+          />
+
+          <SubmitButton onPress={() => { }}>
+            Entrar
+          </SubmitButton>
+        </Form>
+        <SignLink onPress={() => { }}>
+          <SignLinkText>
+            Criar sua conta gratuita
+          </SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 }
